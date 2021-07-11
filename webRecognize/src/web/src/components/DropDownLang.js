ccd4a4efda5d5ae27cb7b813a1ceace2694ca22c
Enzo Aguirre <c.enzoaguirre@gmail.com>
2021-07-11 00:53:41 -0300

    + drop down lang functional

--- webRecognize/src/web/src/components/DropDownLang.js
+++ webRecognize/src/web/src/components/DropDownLang.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import {Translation} from "react-i18next";
 import i18n from "../i18n";
 
-const changeLanguage = (e) => {
+const changeLanguage = (e, setDisplayed) => {
+    setDisplayed(false);
     i18n.changeLanguage(e.target.dataset.lang);
 };
 
 function DropDownLang() {
+    const [displayed, setDisplayed] = useState(false);
     return (
-        <div className="dropdown" id="dropdown-language">
+        <div className={"dropdown" + (displayed ? " is-active" : "")}>
             <div className="dropdown-trigger">
-                <button className="button" aria-haspopup="true" aria-controls="dropdown-menu">
+                <button
+                    className="button"
+                    aria-haspopup="true"
+                    aria-controls="dropdown-menu"
+                    onClick={() => setDisplayed(!displayed)}>
                     <span className="icon is-small">
                         <i className="fas fa-globe" aria-hidden="true"></i>
                     </span>
@@ -22,10 +28,13 @@ function DropDownLang() {
             </div>
             <div className="dropdown-menu" id="dropdown-menu" role="menu">
                 <div className="dropdown-content">
-                    <div className="dropdown-item is-active" data-lang="en" onClick={changeLanguage}>
+                    <div
+                        className="dropdown-item is-active"
+                        data-lang="en"
+                        onClick={(e) => changeLanguage(e, setDisplayed)}>
                         English
                     </div>
-                    <div className="dropdown-item" data-lang="es" onClick={changeLanguage}>
+                    <div className="dropdown-item" data-lang="es" onClick={(e) => changeLanguage(e, setDisplayed)}>
                         Español
                     </div>
                 </div>
