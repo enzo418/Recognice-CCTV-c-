162c04145bdef68cec590174e37f51e935f3d96a
Enzo Aguirre <c.enzoaguirre@gmail.com>
2021-10-24 02:37:11 -0300

    Implemented FrameDisplay class, shows the frames.

--- recognize/src/FrameDisplay.cpp
+++ recognize/src/FrameDisplay.cpp
@@ -1,12 +1,32 @@
 #include "FrameDisplay.hpp"
+#include "SimpleBlockingQueue.hpp"
 
 namespace Observer {
+    FrameDisplay::FrameDisplay(int total) : maxFrames(total) {
+        this->running = false;
+        this->frames.reserve(total);
+    }
+
     void FrameDisplay::Start() {
-        // TODO: Show frames
+        this->running = true;
+
+        std::vector<cv::Mat> framesToShow(maxFrames);
+        while(this->running) {
+            for(int i = 0; i < this->maxFrames; i++) {
+                framesToShow[i] = this->frames[i].pop();
+            }
+
+            // TODO: Show frames
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        }
+    }
+
+    void FrameDisplay::Stop() {
+        this->running = false;
     }
 
     void FrameDisplay::update(int cameraPos, cv::Mat frame) {
-        // TODO: Lock vector
-        this->frames[cameraPos] = frame;
+        this->frames[cameraPos].push(frame);
     }
 }
--- recognize/src/FrameDisplay.hpp
+++ recognize/src/FrameDisplay.hpp
@@ -3,9 +3,40 @@
 #include "BaseObserverPattern.hpp"
 #include "Configuration.hpp"
 
+#include <thread>
+#include <chrono>
+#include <mutex>
+
 namespace Observer {
     class FrameEventSubscriber : public ISubscriber<int, cv::Mat> {
         virtual void update(int camerapos, cv::Mat frame) = 0;
+    };
+
+    template <typename T>
+    class FrameQueue : protected SimpleBlockingQueue<T> {
+        public:
+            virtual void push(T const &value);
+            virtual size_t size();
+
+            T pop()  {
+                super::mutex.lock();
+
+                T elment(std::move(super::queue.back()));
+
+                // if only 1 frames is left, don't delete it
+                if (super::queue.size() > 1) {
+                    super::queue.pop();
+                }
+
+                super::mutex.unlock();
+
+                return elment;
+            }
+
+            virtual ~FrameQueue();
+
+        private:
+            using super = SimpleBlockingQueue<T>;
     };
 
     /**
@@ -14,11 +45,28 @@ namespace Observer {
     class FrameDisplay : public FrameEventSubscriber
     {
         public:
+            /**
+             * @param total Number of frames to display at the same time
+             */
+            FrameDisplay(int total);
+
             void Start();
 
-            void update(int camerapos, cv::Mat frame) override;
+            void Stop();
+
+            /**
+             * @brief add a new frame to the available frames
+             *
+             * @param framePosition 0 = top left, 1 = top right, ...
+             */
+            void update(int framePosition, cv::Mat frame) override;
 
         private:
-            std::vector<cv::Mat> frames;
+            std::vector<FrameQueue<cv::Mat>> frames;
+            std::mutex mtxFrames;
+
+            int maxFrames;
+
+            bool running;
     };
 }
