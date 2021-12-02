e4d67e9f047ae759e9a8ad76884c983776c40bd6
Enzo Aguirre <c.enzoaguirre@gmail.com>
2021-12-02 17:34:57 -0300

    FrameProcessor now uses generic image transformation

--- recognize/src/FrameProcessor.cpp
+++ recognize/src/FrameProcessor.cpp
@@ -1,40 +1,4 @@
 #include "FrameProcessor.hpp"
 
 namespace Observer {
-    FrameProcessor::FrameProcessor(cv::Rect roi, double noiseThreshold) {
-        this->roi = roi;
-        this->noiseThreshold = noiseThreshold;
-    }
-
-    FrameProcessor& FrameProcessor::NormalizeFrame(cv::Mat& frame) & {
-        cv::resize(frame, frame, this->resolutionSize);
-
-        // TODO: RotateImage(this->frame, this->config->rotation);
-
-        // crop the frame
-        if (!this->roi.empty()) {
-            frame = frame(this->roi);
-        }
-
-        // to black and white
-        cv::cvtColor(frame, frame, cv::COLOR_RGB2GRAY);
-
-        // get the difference between the current and last frame
-        cv::absdiff(this->lastFrame, frame, this->diffFrame);
-
-        // make the changes bigger
-        cv::GaussianBlur(this->diffFrame, this->diffFrame, cv::Size(3, 3), 10);
-
-        // remove small changes
-        cv::threshold(this->diffFrame, this->diffFrame, this->noiseThreshold,
-                      255, cv::THRESH_BINARY);
-
-        frame.copyTo(this->lastFrame);
-
-        return *this;
-    }
-
-    double FrameProcessor::DetectChanges() {
-        return cv::countNonZero(this->diffFrame);
-    }
 }  // namespace Observer
--- recognize/src/FrameProcessor.hpp
+++ recognize/src/FrameProcessor.hpp
@@ -2,30 +2,88 @@
 
 #include <opencv2/opencv.hpp>
 
+#include "ImageTransformation.hpp"
+#include "Rect.hpp"
+
 namespace Observer {
+    template <typename T>
     class FrameProcessor {
        private:
-        const cv::Size resolutionSize = cv::Size(640, 360);
+        const Size resolutionSize = Size(640, 360);
 
         // last frame readed
-        cv::Mat lastFrame;
+        T lastFrame;
 
         // holder for the diff frame
-        cv::Mat diffFrame;
+        T diffFrame;
 
         //
         double accumulatorThresholds;
 
-        cv::Rect roi;
+        Rect roi;
 
         double noiseThreshold;
 
-       public:
-        FrameProcessor(cv::Rect roi, double noiseThreshold);
+        double rotation;
 
-        FrameProcessor& NormalizeFrame(cv::Mat& frame) &;
+       public:
+        /**
+         * @brief Construct a new Frame Processor object
+         * 
+         * @param roi region of interest to crop
+         * @param noiseThreshold 
+         * @param rotation angle to rotate the image in degrees
+         */
+        FrameProcessor(Rect roi, double noiseThreshold, double rotation);
+
+        FrameProcessor& NormalizeFrame(T& frame) &;
 
         double DetectChanges();
     };
+
+    template <typename T>
+    FrameProcessor<T>::FrameProcessor(Rect pRoi, double pNoiseThreshold, double pRotation) {
+        this->roi = pRoi;
+        this->noiseThreshold = pNoiseThreshold;
+        this->rotation = pRotation;
+    }
+
+    template <typename T>
+    FrameProcessor<T>& FrameProcessor<T>::NormalizeFrame(T& frame) & {
+        ImageTransformation<T>::Resize(frame, frame, this->resolutionSize);
+
+        ImageTransformation<T>::RotateImage(this->frame, this->rotation);
+
+        // crop the frame
+        if (!this->roi.empty()) {
+            ImageTransformation<T>::CropImage(frame, frame, this->roi);
+        }
+
+        // to black and white
+        ImageTransformation<T>::ToColorSpace(
+            frame, frame, ColorSpaceConversion::COLOR_RGB2GRAY);
+
+        // get the difference between the current and last frame
+        ImageTransformation<T>::AbsoluteDifference(this->lastFrame, frame,
+                                                   this->diffFrame);
+
+        // make the changes bigger
+        ImageTransformation<T>::GaussianBlur(this->diffFrame, this->diffFrame,
+                                             3);
+
+        // remove small changes/noise
+        ImageTransformation<T>::Threshold(this->diffFrame, this->diffFrame,
+                                          this->noiseThreshold, 255,
+                                          cv::THRESH_BINARY);
+
+        ImageTransformation<T>::CopyImage(frame, lastFrame);
+
+        return *this;
+    }
+
+    template <typename T>
+    double FrameProcessor<T>::DetectChanges() {
+        return ImageTransformation<T>::CountNonZero(this->diffFrame);
+    }
 
 }  // namespace Observer
