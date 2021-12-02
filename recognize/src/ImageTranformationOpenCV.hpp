5e3af490d0318e26d5464b8a2e503f0822f1c33d
Enzo Aguirre <c.enzoaguirre@gmail.com>
2021-12-02 17:34:03 -0300

    Added more image transformations

--- recognize/src/ImageTranformationOpenCV.hpp
+++ recognize/src/ImageTranformationOpenCV.hpp
@@ -1,4 +1,6 @@
+#include <opencv2/core.hpp>
 #include <opencv2/core/mat.hpp>
+
 #include "ImageTransformation.hpp"
 #include "opencv4/opencv2/opencv.hpp"
 namespace Observer {
@@ -6,9 +8,10 @@ namespace Observer {
     struct ImageTransformation<cv::Mat> {
         static void AddPad(cv::Mat& image, uint8_t top = 0, uint8_t bottom = 0,
                            uint8_t left = 0, uint8_t right = 0) {
-            cv::copyMakeBorder(image, image, top, bottom, left, right, cv::BORDER_CONSTANT);
+            cv::copyMakeBorder(image, image, top, bottom, left, right,
+                               cv::BORDER_CONSTANT);
         }
-        
+
         static cv::Mat HStackPadded(cv::Mat* images, uint8_t arraySize,
                                     uint8_t height) {
             int maxHeight = 0;
@@ -19,10 +22,10 @@ namespace Observer {
             if (height == 0) {
                 // calculate the ideal height (bigger)
                 for (ushort i = 0; i < arraySize; i++) {
-                    if (images[i].rows > maxHeight)
-                        maxHeight = images[i].rows;
+                    if (images[i].rows > maxHeight) maxHeight = images[i].rows;
                 }
-            } else maxHeight = height;
+            } else
+                maxHeight = height;
 
             for (ushort i = 0; i < arraySize; i++) {
                 assert(images[i].dims <= 2);
@@ -37,7 +40,8 @@ namespace Observer {
                 // TODO: LOG?
                 // std::cout << "Exception: " << e.what() << std::endl;
                 // return a black image of some size
-                res = cv::Mat(640 * arraySize, 360, CV_8UC3, cv::Scalar(0, 0, 0)); 
+                res =
+                    cv::Mat(640 * arraySize, 360, CV_8UC3, cv::Scalar(0, 0, 0));
             }
 
             return res;
@@ -53,10 +57,10 @@ namespace Observer {
             if (width == 0) {
                 // calculate the ideal width (bigger)
                 for (uint8_t i = 0; i < arraySize; i++) {
-                    if (images[i].cols > maxWidth)
-                        maxWidth = images[i].cols;
+                    if (images[i].cols > maxWidth) maxWidth = images[i].cols;
                 }
-            } else maxWidth = width;
+            } else
+                maxWidth = width;
 
             for (uint8_t i = 0; i < arraySize; i++) {
                 if (images[i].cols != maxWidth)
@@ -64,7 +68,7 @@ namespace Observer {
             }
 
             cv::vconcat(images, arraySize, res);
-            return res; 
+            return res;
         }
 
         static cv::Mat StackImages(cv::Mat* images, uint8_t arraySize,
@@ -87,7 +91,8 @@ namespace Observer {
             if ((arraySize - count) == 1) {
                 hstacked.push_back(images[count]);
             } else if ((arraySize - count) > 1) {
-                hstacked.push_back(StackImages(&images[count], (arraySize - count), (arraySize - count)));
+                hstacked.push_back(StackImages(
+                    &images[count], (arraySize - count), (arraySize - count)));
             }
 
             // pass 0 since its the width of two images Hstacked.
@@ -99,5 +104,72 @@ namespace Observer {
             cv::Mat r = cv::getRotationMatrix2D(pc, angle, 1.0);
             cv::warpAffine(image, image, r, image.size());
         }
+
+        static void AbsoluteDifference(cv::Mat& source1, cv::Mat& source2,
+                                       cv::Mat& dest) {
+            cv::absdiff(source1, source2, dest);
+        }
+
+        static void GaussianBlur(cv::Mat& source, cv::Mat& dst, int radius) {
+            assert(radius > 0);
+            assert(radius % 2 == 1);
+            cv::GaussianBlur(source, dst, cv::Size(radius, radius), 10);
+        }
+
+        static void ToColorSpace(
+            cv::Mat& source, cv::Mat& dst,
+            int conversionType = ColorSpaceConversion::COLOR_RGB2GRAY) {
+            switch (conversionType) {
+                case COLOR_RGB2GRAY:
+                    conversionType = cv::COLOR_RGB2GRAY;
+                    break;
+                case COLOR_GRAY2RGB:
+                    conversionType = cv::COLOR_GRAY2RGB;
+                    break;
+                case COLOR_HLS2RGB:
+                    conversionType = cv::COLOR_HLS2RGB;
+                    break;
+                case COLOR_RGB2HLS:
+                    conversionType = cv::COLOR_RGB2HLS;
+                    break;
+            }
+
+            cv::cvtColor(source, dst, conversionType);
+        }
+
+        static inline void CopyImage(cv::Mat& source, cv::Mat& dst) {
+            source.copyTo(dst);
+        }
+
+        static void Threshold(cv::Mat& source, cv::Mat& dst, double threshold,
+                              double max, int type) {
+            switch (type) {
+                case THRESHOLD_BINARY:
+                    type = cv::THRESH_BINARY;
+                    break;
+                case THRESHOLD_BINARY_INV:
+                    type = cv::THRESH_BINARY_INV;
+                    break;
+                case THRESHOLD_TRUNC:
+                    type = cv::THRESH_TRUNC;
+                    break;
+                case THRESHOLD_TOZERO:
+                    type = cv::THRESH_TOZERO;
+                    break;
+                case THRESHOLD_TOZERO_INV:
+                    type = cv::THRESH_TOZERO_INV;
+                    break;
+            }
+
+            cv::threshold(source, dst, threshold, max, type);
+        }
+
+        static int CountNonZero(cv::Mat& image) {
+            return cv::countNonZero(image);
+        }
+
+        static inline void CropImage(cv::Mat& source, cv::Mat& dst, Rect& roi) {
+            dst = source(cv::Rect(roi.x, roi.y, roi.width, roi.height));
+        }
     };
-}
+}  // namespace Observer
--- recognize/src/ImageTransformation.hpp
+++ recognize/src/ImageTransformation.hpp
@@ -1,58 +1,83 @@
 #pragma once
 
 #include <opencv2/opencv.hpp>
+
+#include "Size.hpp"
+#include "Rect.hpp"
+
 namespace Observer {
     // Forward declaration of the struct
     template <typename T>
     struct ImageTransformation;
 
+    enum ColorSpaceConversion {
+        // RGB - Gray
+        COLOR_RGB2GRAY = 0,
+        COLOR_GRAY2RGB = 1,
+
+        // RGB - HLS
+        COLOR_HLS2RGB = 2,
+        COLOR_RGB2HLS = 3
+    };
+
+    // function for each threhold type:
+    // https://docs.opencv.org/4.x/d7/d1b/group__imgproc__misc.html#ggaa9e58d2860d4afa658ef70a9b1115576a147222a96556ebc1d948b372bcd7ac59
+    enum ThresholdType {
+        THRESHOLD_BINARY = 0,
+        THRESHOLD_BINARY_INV = 1,
+        THRESHOLD_TRUNC = 2,
+        THRESHOLD_TOZERO = 3,
+        THRESHOLD_TOZERO_INV = 4
+    };
+
     /**
      * @brief This struct handles all the basics transformations
      * of the selected framework/library
-     * @tparam T 
+     * @tparam T
      */
-    template<typename T>
+    template <typename T>
     struct ImageTransformation {
         /**
-        * @brief Stack images Horizontally
-        *
-        * @param images Array of images to stack
-        * @param arraySize Amount of images to concat
-        * @param height Height of each image in the array.
-        * 0 to automatically calculate it.
-        * @return T 
-        */
+         * @brief Stack images Horizontally
+         *
+         * @param images Array of images to stack
+         * @param arraySize Amount of images to concat
+         * @param height Height of each image in the array.
+         * 0 to automatically calculate it.
+         * @return T
+         */
         static T HStackPadded(T* images, uint8_t arraySize, uint8_t height);
 
         /**
-        * @brief Stack images Vertically
-        * 
-        * @param images Array of images to stack
-        * @param arraySize Amount of images to concat
-        * @param width Width of each image in the array. 0 to automatically calculate it.
-        * @return T 
-        */
+         * @brief Stack images Vertically
+         *
+         * @param images Array of images to stack
+         * @param arraySize Amount of images to concat
+         * @param width Width of each image in the array. 0 to automatically
+         * calculate it.
+         * @return T
+         */
         static T VStackPadded(T* images, uint8_t arraySize, uint8_t width = 0);
 
         /**
-        * @brief Stack `arraySize/maxHStack` rows vertically.
-        * The rows are created from first image to last,
-        * where the number of columns is `maxHStack`.
-        * 
-        * @param images Array of images to stack
-        * @param arraySize Number of images to stack
-        * @param maxHStack Number of images to stack horizontally on each row
-        * @return T 
-        */
+         * @brief Stack `arraySize/maxHStack` rows vertically.
+         * The rows are created from first image to last,
+         * where the number of columns is `maxHStack`.
+         *
+         * @param images Array of images to stack
+         * @param arraySize Number of images to stack
+         * @param maxHStack Number of images to stack horizontally on each row
+         * @return T
+         */
         static T StackImages(T* images, uint8_t arraySize,
-                                    uint8_t maxHStack = 2);
+                             uint8_t maxHStack = 2);
 
         /**
-        * @brief Rotates a image
-        * 
-        * @param image image to rotate
-        * @param angle angle, on degrees
-        */
+         * @brief Rotates a image
+         *
+         * @param image image to rotate
+         * @param angle angle, on degrees
+         */
         static void RotateImage(T& image, double angle);
 
         /**
@@ -64,6 +89,91 @@ namespace Observer {
          * @param left Pad to add in the left side of the image
          * @param right Pad to add in the right side of the image
          */
-        static void AddPad(T& image, uint8_t top = 0, uint8_t bottom = 0, uint8_t left = 0, uint8_t right = 0);
+        static void AddPad(T& image, uint8_t top = 0, uint8_t bottom = 0,
+                           uint8_t left = 0, uint8_t right = 0);
+
+        /**
+         * @brief Resize a image
+         *
+         * @param source source image
+         * @param dst destionation image
+         * @param size size
+         */
+        static void Resize(T& source, T& dst, Size& size);
+
+        /**
+         * @brief Calculates the absolute difference element by element
+         * between two images
+         *
+         * @param source1
+         * @param source2
+         * @param dest
+         */
+        static void AbsoluteDifference(T& source1, T& source2, T& dest);
+
+        /**
+         * @brief Blurs an image using a Gaussian filter
+         *
+         * @param source source image
+         * @param dst destination image
+         * @param radius
+         */
+        static void GaussianBlur(T& source, T& dst, int radius);
+
+        /**
+         * @brief Converts an image from one color space to another
+         *
+         * @param source source image
+         * @param dst destination image
+         * @param conversionType space conversion (ColorSpaceConversion)
+         */
+        static void ToColorSpace(
+            T& source, T& dst,
+            int conversionType = ColorSpaceConversion::COLOR_RGB2GRAY);
+
+        /**
+         * @brief Copy a image
+         *
+         * @param source source to be copied
+         * @param dst destination of the copy
+         */
+        static inline void CopyImage(T& source, T& dst);
+
+        /**
+         * @brief Crop a image, no data is copied.
+         * The destionation image pointer will be pointing to the the sub-array
+         * associated with the specified roi.
+         *
+         * @param source image to be cropped
+         * @param dst destionation image
+         * @param roi region of interest
+         */
+        static inline void CropImage(T& source, T& dst, Rect& roi);
+
+        /**
+         * @brief This function applies fixed-level thresholding to a image.
+         * The function is typically used to get a binary image out of a
+         * grayscale image or for removing a noise, that is, filtering out
+         * pixels with too small or too large values. There are several types of
+         * thresholding supported by the function. They are determined by
+         * type parameter. (description from opencv:threshold)
+         *
+         * @param source source image
+         * @param dst destination image
+         * @param threshold
+         * @param max value to use when applying THRESHOLD_BINARY
+         * or THRESHOLD_BINARY_INV
+         * @param type threshold type (ThresholdType)
+         */
+        static void Threshold(T& source, T& dst, double threshold, double max,
+                              int type);
+
+        /**
+         * @brief Count the number of non-zero elements in the image
+         * 
+         * @param image Gray image (SINGLE-CHANNEL)
+         * @return int count
+         */
+        static int CountNonZero(T& image);
     };
-}
+}  // namespace Observer
