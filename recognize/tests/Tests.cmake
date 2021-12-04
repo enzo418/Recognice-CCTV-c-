3f0991b138c2c871149b7a548a79d64e7efcb8bd
Enzo Aguirre <c.enzoaguirre@gmail.com>
2021-12-04 02:15:18 -0300

    added video buffer tests

---
+++ recognize/tests/Tests.cmake
@@ -0,0 +1,19 @@
+cmake_minimum_required(VERSION 3.14)
+
+set(CMAKE_CXX_STANDARD 20)
+
+include(FetchContent)
+FetchContent_Declare(
+    googletest
+    URL https://github.com/google/googletest/archive/609281088cfefc76f9d0ce82e1ff6c30cc3591e5.zip
+)
+
+# For Windows: Prevent overriding the parent project's compiler/linker settings
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+FetchContent_MakeAvailable(googletest)
+
+# clangd:
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
+set(TESTS_SOURCES ${CMAKE_CURRENT_LIST_DIR}/ConfigurationTest.cpp
+                  ${CMAKE_CURRENT_LIST_DIR}/VideoBufferTest.cpp)
---
+++ recognize/tests/VideoBufferTest.cpp
@@ -0,0 +1,67 @@
+#include <gtest/gtest.h>
+
+#include <cstdlib>
+#include <fstream>
+#include <opencv4/opencv2/core/types.hpp>
+
+#include "../src/VideoBuffer.hpp"
+
+// include all the implementations
+#include "../src/implementations/opencv/ImageTransformation.hpp"
+
+
+using namespace Observer;
+
+const int BUFFER_SIZE = 10;
+
+template <typename T>
+class VideoBufferTest : public testing::Test,
+                        public testing::WithParamInterface<const char*> {
+   protected:
+    VideoBufferTest() : buffer(BUFFER_SIZE, BUFFER_SIZE) {}
+
+    VideoBuffer<T> buffer;
+};
+
+// decleare all the types that are going to be tested
+using TestTypes = ::testing::Types<cv::Mat>;
+
+// Templated suit test
+TYPED_TEST_SUITE(VideoBufferTest, TestTypes);
+
+TYPED_TEST(VideoBufferTest, SimpleAdd) {
+    std::vector<TypeParam> images(BUFFER_SIZE * 2);
+
+    for (int i = 0; i < BUFFER_SIZE * 2; i++) {
+        images[i] = ImageTransformation<TypeParam>::BlackImage();
+    }
+
+    for (int i = 0; i < BUFFER_SIZE * 2; i++) {
+        if (i == BUFFER_SIZE / 2) this->buffer.ChangeWasDetected();
+
+        this->buffer.AddFrame(images[i]);
+    }
+
+    auto ev = this->buffer.GetEventFound();
+    auto& frames = ev.GetFrames();
+
+    ASSERT_EQ(frames.size(), BUFFER_SIZE * 2);
+}
+
+TYPED_TEST(VideoBufferTest, OverrideAdd) {
+    const int addTotal = BUFFER_SIZE * 4;
+    
+    for (int i = 0; i < addTotal; i++) {
+        if (i == BUFFER_SIZE) this->buffer.ChangeWasDetected();
+
+        auto img = ImageTransformation<TypeParam>::BlackImage();
+
+        this->buffer.AddFrame(img);
+    }
+
+    auto ev = this->buffer.GetEventFound();
+
+    // expect size to be eq to BUFFER_SIZE in left + BUFFER_SIZE in right,
+    // BUFFER_SIZE * 2
+    ASSERT_EQ(ev.GetFrames().size(), BUFFER_SIZE * 2);
+}
