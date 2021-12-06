2e08fd70dc2cf5c6b9f27ed36ee0316b32b6ab9d
Enzo Aguirre <c.enzoaguirre@gmail.com>
2021-12-06 19:57:52 -0300

    sample main record video

--- recognize/Observer/Observer.cpp
+++ recognize/Observer/Observer.cpp
@@ -12,6 +12,18 @@
 #include "../Observer/src/Domain/Configuration/ConfigurationParser.hpp"
 #include "../Observer/src/Domain/ObserverCentral.hpp"
 #include "../Observer/src/Log/log.hpp"
+#include "src/Domain/Configuration/Configuration.hpp"
+#include "src/Domain/VideoSource.hpp"
+#include "src/Domain/VideoWriter.hpp"
+#include "src/ImageDisplay.hpp"
+#include "src/ImageTransformation.hpp"
+#include "src/Timer.hpp"
+#include "src/Utils/SpecialFunctions.hpp"
+
+void StartObserver(Observer::Configuration* cfg);
+void RecordCamera(Observer::Configuration* cfg);
+
+using FrameType = cv::Mat;
 
 int main(int argc, char** argv) {
     std::string pathConfig;
@@ -42,8 +54,48 @@ int main(int argc, char** argv) {
 
     auto cfg = Observer::ConfigurationParser::ParseYAML(pathConfig);
 
-    Observer::ObserverCentral<cv::Mat> observer(cfg);
+    RecordCamera(&cfg);
+    // StartObserver(&cfg);
+}
+
+void StartObserver(Observer::Configuration* cfg) {
+    Observer::ObserverCentral<FrameType> observer(*cfg);
     observer.Start();
 
     std::this_thread::sleep_for(std::chrono::hours(30));
+}
+
+void RecordCamera(Observer::Configuration* cfg) {
+    Observer::VideoSource<FrameType> source;
+    Observer::VideoWriter<FrameType> writer;
+
+    source.Open(cfg->camerasConfiguration[0].url);
+
+    FrameType frame;
+
+    source.GetNextFrame(frame);
+
+    auto time = Observer::SpecialFunctions::GetCurrentTime();
+    const std::string file = "./" + time + ".mp4";
+    writer.Open(file, 14, writer.GetDefaultCodec(),
+                Observer::ImageTransformation<FrameType>::GetSize(frame));
+
+    Observer::Timer<std::chrono::seconds> timer;
+
+    Observer::ImageDisplay<FrameType>::CreateWindow("Image");
+
+    timer.Start();
+    auto minutes = 60 * 2;
+    while (timer.GetDuration() < minutes) {
+        source.GetNextFrame(frame);
+        writer.WriteFrame(frame);
+        Observer::ImageDisplay<FrameType>::ShowImage("Image", frame);
+        OBSERVER_TRACE("Left: {} seconds", minutes - timer.GetDuration());
+    }
+
+    Observer::ImageDisplay<FrameType>::DestroyWindow("Image");
+    writer.Close();
+    source.Close();
+
+    OBSERVER_INFO("Recording saved at '{}'", file);
 }
