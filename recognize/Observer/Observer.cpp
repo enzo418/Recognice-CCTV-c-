f73896a58a3c7f1cbcd7597383841b84252d986f
Enzo Aguirre <c.enzoaguirre@gmail.com>
2022-01-02 23:02:09 -0300

    restored main

--- recognize/Observer/Observer.cpp
+++ recognize/Observer/Observer.cpp
@@ -73,107 +73,11 @@ int main(int argc, char** argv) {
 
     auto cfg = Observer::ConfigurationParser::ParseYAML(pathConfig);
 
-    cv::namedWindow("image");
-    cv::startWindowThread();
-
-    auto videouri = "./person_2.mp4";
-    auto videostart = 0;
-    auto videoend = 10;
-
-    std::vector<cv::Mat> frames;
-    // 16 FPS
-    frames.reserve((videoend - videostart) * 16);
-
-    // Get the video
-    cv::VideoCapture cap(videouri);
-
-    if (!cap.isOpened()) {
-        std::cout << " no pudo abrir " << std::endl;
-        return 0;
+    if (recognizerMode) {
+        StartObserver(&cfg);
+    } else {
+        RecordCamera(&cfg, recordingMinutes);
     }
-
-    cap.set(cv::CAP_PROP_POS_MSEC, videostart * 1000);
-
-    auto fps = cap.get(cv::CAP_PROP_FPS);
-
-    cv::Size frameSize = cv::Size(cap.get(cv::CAP_PROP_FRAME_WIDTH),
-                                  cap.get(cv::CAP_PROP_FRAME_HEIGHT));
-
-    cv::Size resizeSize = cv::Size(640, 360);
-
-    int ten_percent_of_screen = (resizeSize.width - resizeSize.height) * 0.08;
-
-    ThresholdingParams dayVeryAccurate = {
-        .FramesBetweenDiffFrames = 3,
-        .ContextFrames = 4,
-        .MedianBlurKernelSize = 3,
-        .GaussianBlurKernelSize = 7,
-        .DilationSize = 2,
-        .BrightnessAboveThreshold = 4,
-        .Resize = {.size = Size(640, 360), .resize = false}};
-
-    auto& param = dayVeryAccurate;
-
-    ContoursFilter filter = {
-        .FilterByAverageArea = true,
-        .MinimumArea = ten_percent_of_screen,
-    };
-
-    BlobDetectorParams detectorParams = {
-        .distance_thresh = ten_percent_of_screen,
-        .similarity_threshold = 0.5,
-        .blob_max_life = static_cast<int>(3 * fps),
-    };
-
-    BlobFilters filtersBlob = {.MinimumOccurrences = (int)fps};
-
-    ContoursDetector<cv::Mat> contoursDetector(param, filter);
-    BlobDetector<cv::Mat> detector(detectorParams, filtersBlob,
-                                   contoursDetector);
-
-    int iFrame = 0;
-    int max = videoend;
-    cv::Mat frame;
-
-    while (cap.get(cv::CAP_PROP_POS_MSEC) / 1000 < max) {
-        if (cap.read(frame)) {
-            cv::resize(frame, frame, resizeSize);
-
-            frames.push_back(frame.clone());
-
-            iFrame++;
-        }
-    }
-
-    // for (int i = 0; i < contours.size(); i++) {
-    //     cv::drawContours(frames[i], contours[i], -1, cv::Scalar(0, 0, 255));
-    // }
-
-    std::vector<Observer::Blob> blobs;
-
-    auto conts = contoursDetector.FindContours(frames);
-
-    blobs = detector.FindBlobs(conts);
-
-    BlobGraphics<cv::Mat>::DrawBlobs(frames, blobs);
-
-    int i = 0;
-    while (i < frames.size()) {
-        frame = frames[i];
-
-        cv::imshow("image", frame);
-        cv::waitKey(62);
-        i++;
-    }
-    std::cout << "blobs: " << blobs.size() << std::endl;
-
-    cv::destroyAllWindows();
-
-    // if (recognizerMode) {
-    //     StartObserver(&cfg);
-    // } else {
-    //     RecordCamera(&cfg, recordingMinutes);
-    // }
 }
 
 void StartObserver(Observer::Configuration* cfg) {
