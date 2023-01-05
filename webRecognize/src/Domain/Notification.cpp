e676cf8980cef57e60db53db622e5a623eb3bc70
enzo418 <c.enzoaguirre@gmail.com>
2023-01-05 07:23:55 -0300

    fixed always use the same camera name - updated nolitedb - nolitedb wasn't generating the correct condition

--- webRecognize/src/Domain/Notification.cpp
+++ webRecognize/src/Domain/Notification.cpp
@@ -1,11 +1,14 @@
 #include "Notification.hpp"
 
+#include "Domain/Camera.hpp"
+
 namespace Web::Domain {
     Notification::Notification(const Observer::DTONotification& notification
                                /*, Camera pCamera*/)
         : content(notification.content),
-          groupID(notification.groupID)
-    /*, camera(std::move(pCamera))*/
+          groupID(notification.groupID),
+          camera(notification.cameraName, "")
+
     {
         this->type = Observer::Helpers::Notifications::NOTIFICATION_TYPE_MAP.at(
             (int)notification.type);
